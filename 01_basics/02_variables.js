2ec8e62c2c3e868ee207a49d56efe9a11dc9c8e5
hiiShreya <121947731+hiiShreya@users.noreply.github.com>
2024-01-25 18:17:28 +0000

    datatypes

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,14 @@
+const accId = 144653
+let accMail="shreya@gmail.com"
+var accPass= "12345"
+accCity="Jaipur"
+let accState;
+
+accPass="21212";
+
+
+console.log(accId);
+console.log(accCity);
+console.log(accPass);
+
+console.table([accId, accPass, accMail, accPass, accCity, accState]);
---
+++ 01_basics/03_datatypes.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log(3+3);
+
+let namee="Shreya"; //String data type
+let id=89; //integers(2 to the pwr 53)
+let log= true; //bool
+let u= null; //null=> standalone value;
+
+
+console.table([namee, id, log, u]);
+
+console.log(typeof log);
+
+
+ 
